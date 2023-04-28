31efbf64fc8088e67bb52ca0cd91b5c1ef69a21d
Shanu26-a11y <132086905+Shanu26-a11y@users.noreply.github.com>
2023-04-28 21:42:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const togglebtn ="Version_";
+const togglebtntwo = 1.5;
+const shanu = "Made by_";
+const himan = "Himan_Shanu";
+const sh = document.querySelector();
+
+console.log(togglebtn + togglebtntwo + shanu + himan);
+
